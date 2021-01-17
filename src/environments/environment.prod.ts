fea360f47edbdfa1bd10b2f8d51cca9f65915e99
Merey <mtemirkas@gmail.com>
2021-01-17 18:17:48 +0600

    init blog angular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCUR8rsvU2TJAvvpcbQl1LuR1LS4lrUTOU',
+  fbDbUrl: 'https://angular-blog-6624b-default-rtdb.firebaseio.com/'
+};
